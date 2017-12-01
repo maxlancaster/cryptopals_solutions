c3df02b0a8375d2d1da6b700ff4e30e6a2beb4cb
Maxwell <max.lancaster25@gmail.com>
2017-12-01 17:13:09 -0500

    challenge 4

---
+++ set1/challenge_4.py
@@ -0,0 +1,22 @@
+import challenge_3
+
+def crack_singlechar_xor(file):
+	list_of_bests = []
+	for line in file:
+		line = line.strip()
+		list_of_bests.append(challenge_3.single_byte_xor_decipher(line))
+
+	score = 0
+	best = ''
+	for i in list_of_bests:
+		this_score = challenge_3.score(i)
+		if this_score > score:
+			score = this_score
+			best = i
+	return best
+
+def test():
+	input_file = open('4.txt', 'r')
+	print crack_singlechar_xor(input_file)
+
+test()
